6234e2334113e445b7118e9ed5e14bd910687a53
Travis Sunderland <tssund93@gmail.com>
2014-02-26 15:28:33 -0600

    Start
    bullets need a lot of work. not sure if collision grid is working
    correctly

---
+++ STG/STG/GameObjectManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace STG
+{
+    public class GameObjectManager
+    {
+        List<GameObject> objectList = new List<GameObject>(); //list of all objects in the game
+        List<GameObject> addList = new List<GameObject>(); //list to store objects until they can be added to objectList
+        List<GameObject> removeList = new List<GameObject>(); //list to store objects until they can be removed from objectList
+
+        public GameObjectManager() { }
+
+        public void Add(GameObject o)
+        {
+            addList.Add(o);
+        }
+
+        public void Remove(GameObject o)
+        {
+            removeList.Add(o);
+        }
+
+        public void Update()
+        {
+            foreach (GameObject o in objectList)
+                o.Update();
+            foreach (GameObject o in addList) //now that we're done looping through objectList, we can add new objects to it
+                objectList.Add(o);
+            addList.Clear(); //clears the temp list
+            foreach (GameObject o in removeList) //now that we're done looping through objectList, we can remove objects from it
+                objectList.Remove(o);
+            removeList.Clear();
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            objectList.Sort(delegate(GameObject obj1, GameObject obj2) //sorts actionList by time the action will be performed, that way you only have to check the first item in the list
+            {
+                return obj1.Position.Y.CompareTo(obj2.Position.Y);
+            });
+            foreach (GameObject o in objectList)
+                o.Draw(spriteBatch);
+        }
+    }
+}
---
+++ STG/STG/Player.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace STG
+{
+    public class Player:GameObject
+    {
+        public enum PlayerNum {One, Two};
+        PlayerNum playerNum;
+
+        int cooldown = 0; //frames until another bullet can be fired
+
+        const int speed = 5; //player's speed
+
+        List<Tuple<Bullet.Action, float, int>> actionList = new List<Tuple<Bullet.Action, float, int>>();
+
+        public Player(Texture2D sprite, PlayerNum playerNum, Vector2 pos, int hitboxWidth, int hitboxHeight)
+        {
+            this.sprite = sprite;
+            this.pos = pos;
+            this.boundingBox = new Rectangle((int)pos.X, (int)pos.Y, sprite.Width, sprite.Height);
+            this.hitbox.Width = hitboxWidth;
+            this.hitbox.Height = hitboxHeight;
+            this.playerNum = playerNum;
+        }
+        public Player(Texture2D sprite, PlayerNum playerNum, Rectangle boundingBox, int hitboxWidth, int hitboxHeight)
+        {
+            this.sprite = sprite;
+            this.pos = new Vector2(boundingBox.X, boundingBox.Y);
+            this.boundingBox = boundingBox;
+            this.hitbox.Width = hitboxWidth;
+            this.hitbox.Height = hitboxHeight;
+            this.playerNum = playerNum;
+        }
+        public Player(Texture2D sprite, PlayerNum playerNum, Vector2 pos)
+        {
+            this.sprite = sprite;
+            this.pos = pos;
+            this.boundingBox = new Rectangle((int)pos.X, (int)pos.Y, sprite.Width, sprite.Height);
+            this.hitbox = new Rectangle((int)pos.X, (int)pos.Y, sprite.Width, sprite.Height);
+            this.playerNum = playerNum;
+        }
+        public Player(Texture2D sprite, PlayerNum playerNum, Rectangle boundingBox)
+        {
+            this.sprite = sprite;
+            this.pos = new Vector2(boundingBox.X, boundingBox.Y);
+            this.boundingBox = boundingBox;
+            this.hitbox = new Rectangle((int)pos.X, (int)pos.Y, sprite.Width, sprite.Height);
+            this.playerNum = playerNum;
+        }
+
+        public override void Update()
+        {
+            KeyboardState keyboard = Keyboard.GetState();                
+
+            switch (playerNum)
+            {
+                case PlayerNum.One:
+                    //movement
+                    if (keyboard.IsKeyDown(Keys.Left) && pos.X - sprite.Width / 2 > 0)
+                        pos.X -= speed;
+                    if (keyboard.IsKeyDown(Keys.Down) && pos.Y + sprite.Height / 2 < Game1.windowHeight)
+                        pos.Y += speed;
+                    if (keyboard.IsKeyDown(Keys.Right) && pos.X + sprite.Width / 2 < Game1.windowWidth)
+                        pos.X += speed;
+                    if (keyboard.IsKeyDown(Keys.Up) && pos.Y - sprite.Height / 2 > 0)
+                        pos.Y -= speed;
+
+                    //shootin
+                    if (keyboard.IsKeyDown(Keys.NumPad1) && cooldown == 0)
+                    {
+                        Game1.objectManager.Add(new Bullet(Game1.textureDict["bullet"], pos, new Vector2(0, -20), this, actionList));
+                        cooldown = 2;
+                    }
+                    if (cooldown > 0)
+                        cooldown--;
+
+                    break;
+
+                case PlayerNum.Two:
+                    //movement
+                    if (keyboard.IsKeyDown(Keys.A) && pos.X - sprite.Width / 2 > 0)
+                        pos.X -= speed;
+                    if (keyboard.IsKeyDown(Keys.S) && pos.Y + sprite.Height / 2 < Game1.windowHeight)
+                        pos.Y += speed;
+                    if (keyboard.IsKeyDown(Keys.D) && pos.X + sprite.Width / 2 < Game1.windowWidth)
+                        pos.X += speed;
+                    if (keyboard.IsKeyDown(Keys.W) && pos.Y - sprite.Height / 2 > 0)
+                        pos.Y -= speed;
+
+                    //shootin
+                    if (keyboard.IsKeyDown(Keys.G) && cooldown == 0)
+                    {
+                        Game1.objectManager.Add(new Bullet(Game1.textureDict["bullet"], pos, new Vector2(0, 20), this, actionList));
+                        cooldown = 2;
+                    }
+                    if (cooldown > 0)
+                        cooldown--;
+
+                    break;
+            }
+
+            foreach (GameObject o in CollidingWith())
+            {
+                if (o.GetType() == typeof(Bullet))
+                {
+                    Bullet t = (Bullet)o; //make bullet variable t so you can check it's parent
+                    if (t.Parent != this)
+                        Game1.objectManager.Remove(o);
+                }
+            }
+
+            base.Update();
+        }
+
+        public int Speed { get { return speed; } }
+    }
+}
