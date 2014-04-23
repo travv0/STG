b38f207cfd483e001259711720e4293f290bcab2
MegaGuts549 <rommer711@yahoo.com>
2014-04-23 18:27:11 -0500

    Bomb Bullet Removal Idea in the works

--- STG/STG/MainGame.cs
+++ STG/STG/MainGame.cs
@@ -141,6 +141,7 @@ namespace STG
 
             //Bomb Texture
             SpriteDict["bombRad"] = new Sprite(Content.Load<Texture2D>("bombRad"));
+            SpriteDict["bombInt"] = new Sprite(Content.Load<Texture2D>("bombInt"));
 
             //Item textures
             SpriteDict["FullItem"] = new Sprite(Content.Load<Texture2D>("Item sprites\\FullItem"));
