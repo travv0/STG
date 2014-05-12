a4e1b85ba165c377dddc1f57a420f76f983b56ac
samiammercer <samiammercer@gmail.com>
2014-05-12 00:00:36 -0500

    fix column and row size

--- STG/STG/Collision.cs
+++ STG/STG/Collision.cs
@@ -15,7 +15,7 @@ namespace STG
     public class Collision
     {
         const int ROWS = 10, COLUMNS = 10;
-        static int CELLWIDTH = (MainGame.PlayingArea.Width + 100) / COLUMNS, CELLHEIGHT = (MainGame.PlayingArea.Height + 100) / ROWS;//constants for the size of the collision grid as well as the cell dimensions
+        static int CELLWIDTH = (MainGame.PlayingArea.Width + 100) / ROWS, CELLHEIGHT = (MainGame.PlayingArea.Height + 100) / COLUMNS;//constants for the size of the collision grid as well as the cell dimensions
         public HashSet<GameObject>[,] collisionGrid = new HashSet<GameObject>[ROWS, COLUMNS];//list of the gameobjects in each square of the collision grid
         List<GameObject> addList = new List<GameObject>(); //list to store objects until they can be added to the collision grid
         List<GameObject> removeList = new List<GameObject>(); //list to store objects until they can be removed from the collision grid
