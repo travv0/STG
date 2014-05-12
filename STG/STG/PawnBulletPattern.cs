4c95f7a335f31f5ba0b95413b76b8fc9d07833d2
Travis <tssund93@gmail.com>
2014-05-11 22:57:41 -0500

    added AI, working on bullet patterns

---
+++ STG/STG/PawnBulletPattern.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace STG
+{
+    public class PawnBulletPattern : BulletPattern
+    {
+        List<Tuple<Bullet.Action, float, int, int, bool>> actionList = new List<Tuple<Bullet.Action, float, int, int, bool>>();
+
+        public PawnBulletPattern(GameObject parent, Sprite bulletSprite)
+        {
+            this.parent = parent;
+            this.pos = parent.Position;
+        }
+
+        protected override void Initialize()
+        {
+ 	        base.Initialize();
+        }
+
+        public override void Update()
+        {
+            for (int i = 0; i < 360; i += 10)
+            {
+                MainGame.ObjectManager.Add(new Bullet(sprite, new Vector2(Position.X, Position.Y), 10, i, 0, parent, actionList));
+            }
+
+            for (int i = 5; i < 365; i += 10)
+            {
+                MainGame.ObjectManager.Add(new Bullet(sprite, new Vector2(Position.X, Position.Y), 10, i, 0, parent, actionList));
+            }
+
+            MainGame.ObjectManager.Remove(this);
+            
+            base.Update();
+        }
+    }
+}
